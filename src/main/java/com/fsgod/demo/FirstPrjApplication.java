69e5e2c09cfc35ae77a6e25b8ca3a7676c3b859e
FSGod <fsGod@>
2021-09-08 00:28:05 +0900

    init prj

---
+++ src/main/java/com/fsgod/demo/FirstPrjApplication.java
@@ -0,0 +1,13 @@
+package com.fsgod.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsgod/demo/FirstPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsgod.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
